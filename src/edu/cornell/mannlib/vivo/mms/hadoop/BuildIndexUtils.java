bbc5fd60fcd5aa013354fc53a4095a72a3f29be2
Brian Caruso <bdc34@cornell.edu>
2013-03-20 10:56:01 -0400

    adding BuildIndexUtils.java

---
+++ src/edu/cornell/mannlib/vivo/mms/hadoop/BuildIndexUtils.java
@@ -0,0 +1,16 @@
+package edu.cornell.mannlib.vivo.mms.hadoop;
+
+public class BuildIndexUtils {
+
+	// these can be used with conuters from the context
+	//ex. context.getCounter(counters.URIS_DISCOVERED).increment(1);
+	public static enum coutners {
+		   URIS_DISCOVERED,			
+		   HTTP_LD_REQUESTS,
+			SOLR_DOCS_INDEXED
+	};
+	
+	//public static Path getWorkingDir(config? context?)
+	
+	
+}
