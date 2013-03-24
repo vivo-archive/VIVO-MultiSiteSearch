5d384568e8facda5f584a0bb530ef5a887b8c01d
j2blake <jeb228@cornell.edu>
2013-03-24 14:42:03 -0400

    Include a pointer to the Site Configuration file

--- src/edu/cornell/mannlib/vivo/mms/hadoop/BuildIndexUtils.java
+++ src/edu/cornell/mannlib/vivo/mms/hadoop/BuildIndexUtils.java
@@ -9,7 +9,12 @@ public class BuildIndexUtils {
 
     /** Number of http client connections per route (per mapper task) when getting RDF */
     public static final String connectionsPerRoute = "connectionsPerRoute";
+    
+    /** Which implementation of DiscoverUrisForSite should we use? */
 	public static final String discoveryImpl = "BuildIndex.discoveryImpl";
+	
+	/** Where is the site Configuration file located? */
+	public static final String siteConfigLocation = "BuildIndex.siteConfiguration";
 	
 	// these can be used with counters from the context
 	//ex. context.getCounter(Counters.URIS_DISCOVERED).increment(1);
@@ -19,6 +24,7 @@ public class BuildIndexUtils {
 		   HTTP_LD_CACHE_MISSES,
 		   SOLR_DOCS_INDEXED
 	}
+
 	
 	
 	//public static Path getWorkingDir(config? context?)
