fecd3536b333fd8cf4ac9b3621388f3e63bdf9e9
Brian Caruso <bdc34@cornell.edu>
2013-03-29 17:30:19 -0400

    ExpandingLinkedDataService passes tests

--- test/edu/cornell/mannlib/vivo/mms/linkedData/LinkedDataExpanderTest.java
+++ test/edu/cornell/mannlib/vivo/mms/linkedData/LinkedDataExpanderTest.java
@@ -1,31 +1,44 @@
 package edu.cornell.mannlib.vivo.mms.linkedData;
 
+import static edu.cornell.mannlib.vivo.mms.linkedData.LinkedDataTestUtils.*;
+
 import static org.junit.Assert.*;
 
-import java.io.InputStream;
-
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 
+import edu.cornell.mannlib.vivo.mms.utils.Log4JHelper;
+
 public class LinkedDataExpanderTest {
 
-    @Test
-    public void test() {
-        String exampleRdf = "edu/cornell/mannlib/vivo/mms/linkedData/resources/bdc34.n3";
-         InputStream in = 
-            this.getClass().getClassLoader().getResourceAsStream(exampleRdf);
-        assertNotNull("Could not read in " +exampleRdf , in);
+    @BeforeClass
+    public static void setUpClass() {
+        Log4JHelper.resetToConsole();
+    }
 
-        Model m = ModelFactory.createDefaultModel();
-        m.read( in, null , "N3" );
+    @Test
+    public void test() throws Exception{
+
+        Model sourceModel = getBdc34TestModel();
         assertTrue("The testing model created from bdc34.n3 should have "+
-                   "some statements" , m.size() > 0 );
+                   "some statements" , sourceModel.size() > 0 );
         
-        LinkedDataService innerLDService = new ModelLinkedDataService( m );
-        LinkedDataService testService = new ExpandingLinkedDataService( innerLDService, new UrisToExpand() );
+        LinkedDataService innerLDService = 
+            new ModelLinkedDataService( sourceModel );
+        LinkedDataService expandingLDService = 
+            new ExpandingLinkedDataService( innerLDService, new UrisToExpand() );
         
+        Model expandedModel = ModelFactory.createDefaultModel();
+        assertTrue("should be empty", expandedModel.size() == 0 );
+        expandingLDService.getLinkedData( URI_BDC34, expandedModel);
+        assertTrue("should now have some statements " , expandedModel.size() > 0 );
+        
+        //Check that there are one and two hop predicates
+        assertTrue("should have one hop statements", hasOneHopStatementsForUri(expandedModel,URI_BDC34));
+        assertTrue("should have two hop statements", hasTwoHopStatementsForUri(expandedModel,URI_BDC34));
     }
 
 }
--- test/edu/cornell/mannlib/vivo/mms/linkedData/ModelLinkedDataServiceTest.java
+++ test/edu/cornell/mannlib/vivo/mms/linkedData/ModelLinkedDataServiceTest.java
@@ -2,8 +2,6 @@ package edu.cornell.mannlib.vivo.mms.linkedData;
 
 import static org.junit.Assert.*;
 
-import java.io.InputStream;
-
 import org.junit.Test;
 
 import com.hp.hpl.jena.query.Query;
@@ -12,51 +10,45 @@ import com.hp.hpl.jena.query.QueryExecutionFactory;
 import com.hp.hpl.jena.query.QueryFactory;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import static edu.cornell.mannlib.vivo.mms.linkedData.LinkedDataTestUtils.*;
 
 public class ModelLinkedDataServiceTest{
-    static String URI_BDC34 = "http://vivo.cornell.edu/individual/individual19589";
-    static String bdc34ExampleRdf = "edu/cornell/mannlib/vivo/mms/linkedData/resources/bdc34.n3";
 
     @Test
     public void test() throws Exception{
-         InputStream in = 
-            this.getClass().getClassLoader().getResourceAsStream(bdc34ExampleRdf);
-        assertNotNull("Could not read in " +bdc34ExampleRdf , in);
 
-        Model sourceModel = ModelFactory.createDefaultModel();
-        sourceModel.read( in, null , "N3" );
+        Model sourceModel = getBdc34TestModel();
         assertTrue("The testing model created from bdc34.n3 should have "+
                    "some statements" , sourceModel.size() > 0 );
+
+
+        
+        // check that one and two hop tests work
+        assertTrue( hasOneHopStatementsForUri( sourceModel, URI_BDC34));
+        assertTrue( hasTwoHopStatementsForUri( sourceModel, URI_BDC34));
+
         
         LinkedDataService ldService = new ModelLinkedDataService( sourceModel );
         
-        Model outputModel = ModelFactory.createDefaultModel();
-        assertTrue("should be empty", outputModel.size() == 0 );
-        ldService.getLinkedData( URI_BDC34, outputModel);
-        assertTrue("should now have some statements " , outputModel.size() > 0 );
+        Model model = ModelFactory.createDefaultModel();
+        assertTrue("should be empty", model.size() == 0 );
+        ldService.getLinkedData( URI_BDC34, model);
+        assertTrue("should now have some statements " , model.size() > 0 );
        
 
         // Check that we have statements that are one step away from the individual
         // but not two steps away. There are two statements two edges away from
         // URI_BDC34 but the ModelLinkedDataService only returns one step
-        // triples when getLinkedData() is called. 
+        // triples when getLinkedData() is called. This is the desired behavior
+        // since the ModelLinkedDataService is intended to test the 
+        // linked data expander. 
 
-        String queryString = "ASK  { <"+ URI_BDC34 + "> ?p ?o }";
-        Query query = QueryFactory.create(queryString) ;
-        QueryExecution qexec = QueryExecutionFactory.create(query, outputModel) ;
-        boolean result = qexec.execAsk() ;
-        qexec.close() ;
+        
+        assertTrue( hasOneHopStatementsForUri( model, URI_BDC34) );
 
-        assertTrue( result );
 
         // check that there are no two hop predicates
+        assertTrue( ! hasTwoHopStatementsForUri(model, URI_BDC34) );
 
-        String queryString2 = "ASK  { <"+ URI_BDC34 + "> ?p ?o . ?o ?p2 ?o2 }";
-        query = QueryFactory.create(queryString2);
-        qexec = QueryExecutionFactory.create(query, outputModel);
-        boolean hasSecondHopPredicates = qexec.execAsk();
-        qexec.close();
-
-        assertTrue( ! hasSecondHopPredicates );
     }
 }
