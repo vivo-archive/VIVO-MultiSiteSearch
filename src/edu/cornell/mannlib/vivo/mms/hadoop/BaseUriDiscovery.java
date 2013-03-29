b89ef6c2c20c5a20a83920e52ce0264273d7dc9d
Brian Caruso <bdc34@cornell.edu>
2013-03-29 16:12:35 -0400

    Adding example of BaseUriDiscovery

--- src/edu/cornell/mannlib/vivo/mms/hadoop/BaseUriDiscovery.java
+++ src/edu/cornell/mannlib/vivo/mms/hadoop/BaseUriDiscovery.java
@@ -7,12 +7,16 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.w3c.dom.Document;
 
-import edu.cornell.mannlib.vivo.mms.discovery.DiscoverUrisContext;
 import edu.cornell.mannlib.vivo.mms.discovery.DiscoverUrisForSite;
-import edu.cornell.mannlib.vivo.mms.utils.HadoopContextHelper;
 import edu.cornell.mannlib.vivo.mms.utils.Log4JHelper;
+
+
+/*
+ * This is a suggestion of how something like this could be done.
+ * Sub classes of this would define how to make the DiscoverUrisForSite
+ * object by directly constructing it.
+ */
 
 /**
  * Get all the URIs for a given site.
@@ -26,42 +30,31 @@ import edu.cornell.mannlib.vivo.mms.utils.Log4JHelper;
  * "http://vivo.cornell.edu/indiviudal134" ... ]
  */
 public abstract class BaseUriDiscovery extends Mapper<LongWritable, Text, Text, Text> {
-	Log log = LogFactory.getLog(UriDiscovery.class);
+	Log log = LogFactory.getLog(BaseUriDiscovery.class);
 
 	private DiscoverUrisForSite uriSource;
-
-    protected abstract DiscoverUrisForSite getDiscoverUrisForSite( Context context );
 
 	@Override
 	protected void setup(Context context) throws IOException,
 			InterruptedException {
 		Log4JHelper.addConfigfile("log4j.job.properties");
 		super.setup(context);
-		this.uriSource = getDiscoverUrisForSite(context);            
+
+        throw new Error("This interface doesn't work any more");
+		//this.uriSource = getDiscoverUrisForSite(context);
 	}
 
 	@Override
 	protected void map(LongWritable lineNum, Text urlOfSite, Context context)
-			throws IOException, InterruptedException {
-		DiscoverUrisContext duContext;
-		try {
-			duContext = DiscoverUrisContext.wrap(context, siteConfigDoc);
-		} catch (Exception e) {
-			throw new Error("Failed to create the DiscoverUrisContext.", e);
-		}
-
-		String siteUrl = urlOfSite.toString();
-		for (String uri : uriSource.getUrisForSite(siteUrl, duContext)) {
-			context.getCounter(BuildIndexUtils.Counters.URIS_DISCOVERED)
-					.increment(1);
-			context.write(urlOfSite, new Text(uri));
-		}
+			throws IOException, InterruptedException {		
+		
+		// String siteUrl = urlOfSite.toString();
+		// for (String uri : uriSource.getUrisForSite(siteUrl)) {
+		// 	context.getCounter(BuildIndexUtils.Counters.URIS_DISCOVERED)
+		// 			.increment(1);
+		// 	context.write(urlOfSite, new Text(uri));
+		// }
 	}
-
-
-    public static DiscoverUrisForSite setupDiscoverUrisForSite(
-        String siteUrl, HttpClient httpClient, List<String>classUris){
-
-    }
-                                                               
+   
+    //public abstract DiscoveryUrisForSite getDiscoverUrisForSite( Context context );
 }
--- src/edu/cornell/mannlib/vivo/mms/hadoop/VivoUriDiscovery.java
+++ src/edu/cornell/mannlib/vivo/mms/hadoop/VivoUriDiscovery.java
@@ -2,62 +2,43 @@ package edu.cornell.mannlib.vivo.mms.hadoop;
 
 import java.io.IOException;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Mapper;
 import org.w3c.dom.Document;
 
 import edu.cornell.mannlib.vivo.mms.discovery.DiscoverUrisContext;
 import edu.cornell.mannlib.vivo.mms.discovery.DiscoverUrisForSite;
 import edu.cornell.mannlib.vivo.mms.utils.HadoopContextHelper;
-import edu.cornell.mannlib.vivo.mms.utils.Log4JHelper;
 
-/**
- * Get all the URIs for a given site.
+/*
+ * This is a suggestion of how something like this could be done.
+ * This subclass of BaseUriDiscovery gets the info it needs
+ * out of the haddop Context and constructs any objects it needs.
  * 
- * INPUT: This mapper expects Text inputs that are URLs of sites. No keys are
- * expected. ex. [ "http://vivo.cornell.edu" ]
- * 
- * OUTPUT: URIs of individuals from the sites that should be added to the index.
- * The key should be the site URL and the value should be the URI of an
- * individual from that site. ex. [ "http://vivo.cornell.edu" :
- * "http://vivo.cornell.edu/indiviudal134" ... ]
+ * When doing this think "simple".  When you wonder how do I get
+ * the list of ISO-2332 formats to the ISO2332HttpResponseTransformer
+ * try the following in this order: 
+ * 1. as defaults in the class ISO2332HttpResponseTransformer 
+ * 2. via the constructor to the class ISO2332HttpResponseTransformer 
+ * 3. via setters to ISO2332HttpResponseTransformer
+ * 4. figure out why you did like 1-3 and do a check to see if you are
+ *    going down some unecessary customization rabbit hole.
  */
-public class VivoUriDiscovery extends UriDiscovery {
 
-	private DiscoverUrisForSite uriSource;
-	private Document siteConfigDoc;
+public class VivoUriDiscovery extends BaseUriDiscovery {
 
-
-	@Override
-	protected void map(LongWritable lineNum, Text urlOfSite, Context context)
-			throws IOException, InterruptedException {
-		DiscoverUrisContext duContext;
-		try {
-			duContext = DiscoverUrisContext.wrap(context, siteConfigDoc);
-		} catch (Exception e) {
-			throw new Error("Failed to create the DiscoverUrisContext.", e);
-		}
-
-		String siteUrl = urlOfSite.toString();
-		for (String uri : uriSource.getUrisForSite(siteUrl, duContext)) {
-			context.getCounter(BuildIndexUtils.Counters.URIS_DISCOVERED)
-					.increment(1);
-			context.write(urlOfSite, new Text(uri));
-		}
-	}
 
     protected DiscoverUrisForSite getDiscoverUrisForSite( Context context ){
-        return 
-            HadoopContextHelper
-            .instantiateFromProperty(context,BuildIndexUtils.discoveryImpl, DiscoverUrisForSite.class);
+
+        //String transformLevel = context.getConfig().get(BuildIndexUtils.Iso2332TransformLevel);
+
+        //return new VivoDiscoverUrisForSite( 
+        //  new HttpWorkerImpl( HdfsHttpClientCacheStorage( context.getFs() ),
+        //  new DiscoverUrisUsingSearchPages( ), 
+        //  new ISO2332HttpResponseTransformer( transformLevel ) ,
+        //);
+
+        throw new Error("This is unimplemented.");
     }
 
-    protected Document getSiteConfigDoc( Context context ){
-        return             
-            HadoopContextHelper.parseXmlFileAtProperty(context,
-				BuildIndexUtils.siteConfigLocation);
-    }
 }
